767e51833689e38bab3f874fab04f43cdeddf02b
menu-made <31354115+JPBattaglia@users.noreply.github.com>
2026-03-04 16:53:18 -0800

    Add files via upload

---
+++ create-checkout-session.js
@@ -0,0 +1,40 @@
+const Stripe = require("stripe");
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event) => {
+
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405 };
+  }
+
+  const data = JSON.parse(event.body);
+
+  try {
+
+    const session = await stripe.checkout.sessions.create({
+      mode: "payment",
+
+      line_items: data.items.map(item => ({
+        price: item.priceId,
+        quantity: item.quantity
+      })),
+
+      success_url: process.env.URL + "/checkoutsuccess.html",
+      cancel_url: process.env.URL + "/checkoutcancel.html"
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ url: session.url })
+    };
+
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message })
+    };
+
+  }
+};
